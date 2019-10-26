76b1b4de1a80ec1c135011fb4368eebba43943a3
MonsieurWai <monsieurwai@gmail.com>
2019-10-26 17:25:57 +0200

    sapper with sass/scss

---
+++ sft-app/src/routes/_layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Nav from '../components/Nav.svelte';
+
+	export let segment;
+</script>
+
+<style lang="scss" global>
+ @import "./style/global.scss";
+</style>
+
+<Nav {segment}/>
+
+<main>
+	<slot></slot>
+</main>
